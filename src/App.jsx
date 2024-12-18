aba46f94bd31228fdc0d088f2d5e7742dc795899
Harsh Anand <harsh250600@gmail.com>
2024-12-18 14:02:38 +0530

    Component App.jsx started to develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState();
+  const [todoList, setTodoList] = useState([]);
+
+  const onInputChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const onAddTodoClick = () => {
+    setTodoList([...todoList, { id: uuid(), todo: todo }]);
+    setTodo("");
+  };
+
+  console.log(todoList);
 
   return (
-    <>
+    <div className="App">
+      <h1>My Wishlist</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          value={todo}
+          onChange={onInputChange}
+          placeholder="Add your wishlist here..."
+        />
+        <button onClick={onAddTodoClick}>Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        {todoList &&
+          todoList.length > 0 &&
+          todoList.map((todo) => (
+            <div key={todo.id}>
+              <label>
+                <input type="checkbox" />
+                <span>{todo.todo}</span>
+              </label>
+              <button>Delete</button>
+            </div>
+          ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
